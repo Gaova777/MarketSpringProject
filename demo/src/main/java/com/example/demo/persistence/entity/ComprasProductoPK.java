93fd466815dcf269dfd34b00afe3ca674dab574f
Juan Diego Garzon Ovalle <gaova777@utp.edu.co>
2024-03-17 19:55:43 -0500

    15_RelationsOfEntitys

---
+++ demo/src/main/java/com/example/demo/persistence/entity/ComprasProductoPK.java
@@ -0,0 +1,32 @@
+package com.example.demo.persistence.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+
+@Embeddable
+public class ComprasProductoPK implements Serializable {
+
+    @Column(name="id_compra")
+    private Integer idCompra;
+
+    @Column(name="id_productos")
+    private Integer idProductos;
+
+
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public void setIdCompra(Integer idCompra) {
+        this.idCompra = idCompra;
+    }
+
+    public Integer getIdProductos() {
+        return idProductos;
+    }
+
+    public void setIdProductos(Integer idProductos) {
+        this.idProductos = idProductos;
+    }
+}
